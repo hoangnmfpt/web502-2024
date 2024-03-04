27b7ef23c6f2ec7ecf16b5eea552fb7c090e4ac1
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-04 13:55:33 +0700

    add lab

--- lesson_1/index.ts
+++ lesson_1/index.ts
@@ -2,5 +2,21 @@ const myStudents = ["Hoang", "Hue", "Hoa", "Chinh"];
 
 const myName = "Nguyễn Minh Hoàng";
 
-console.log(myStudents);
-console.log(myName);
+// const myInfor:string = "ten cua toi la abc"
+const myInfor = {
+  name: "Nguyễn Minh Hoàng",
+  age: 20 + 12,
+  address: "Bắc Giang",
+  isStudent: false,
+  gender: "male",
+  isMarried: true,
+};
+
+const a = 10;
+
+const b = 20;
+
+const sum = (x, y) => x + y;
+console.log(sum(a, b));
+
+myInfor;
