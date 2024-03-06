348dd31b7ee2435ab8624dfdd703a81390c2e9df
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-06 16:00:23 +0700

    Lesson 2: Config format code

--- ts/lesson_2/test.ts
+++ ts/lesson_2/test.ts
@@ -1,33 +1,34 @@
 {
   const myCV = {
-    fullName: "Nguyen Minh Hoang",
-    address: "Bắc Giang city",
+    fullName: 'Nguyen Minh Hoang',
+    address: 'Bắc Giang city',
     age: 32,
     isMarried: true,
-    email: "hoangnm62@fpt.edu.vn",
+    email: 'hoangnm62@fpt.edu.vn',
     education: {
-      university: "FPT University",
-      major: "Software Engineering",
+      university: 'FPT University',
+      major: 'Software Engineering',
       graduatedYear: 2013,
+
       GPA: 3.2,
     },
-    skills: ["HTML-CSS", "Javascript", "ReactJS", "NodeJS", "MongoDB"],
+    skills: ['HTML-CSS', 'Javascript', 'ReactJS', 'NodeJS', 'MongoDB'],
     projects: [
       {
         id: 1,
-        name: "E-commerce website",
-        duration: "6 months",
-        description: "Build a website for selling clothes",
+        name: 'E-commerce website',
+        duration: '6 months',
+        description: 'Build a website for selling clothes',
       },
       {
         id: 1,
-        name: "Music player",
-        duration: "2 months",
-        description: "Website for listening music",
+        name: 'Music player',
+        duration: '2 months',
+        description: 'Website for listening music',
       },
     ],
   };
   console.log(myCV);
 }
 
-console.log("Hello!");
+console.log('Hello!');
