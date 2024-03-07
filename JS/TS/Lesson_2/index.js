dd486f60752d3f287ef8133de075c0c6cd17aa7c
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-07 08:49:48 +0700

    Lesson 2: khai bao với :

--- JS/TS/Lesson_2/index.js
+++ JS/TS/Lesson_2/index.js
@@ -1,2 +1,15 @@
 "use strict";
+/**
+ * Khai báo kiểu dữ liệu với ":" cho string, number, blooean, object, array
+ */
 const myMessage = "Hom nay troi dep qua, rat vui duoc noi chuyen voi ban!";
+const myAge = 30;
+const isMarried = false;
+const product = {
+    name: "Iphone 12",
+    price: 1000,
+    isAvailable: true,
+    thumbnail: "https://example.com/iphone12.jpg",
+};
+const myStudents = ["Dang", "Nam", "Bach", "Hoa", "Xanh"]; // Khai báo kiểu array type
+const myStudents2 = ["Dang", "Nam", "Bach", "Hoa", "Xanh"]; // Khai báo kiểu generic type
--- TS/Lesson_2/index.ts
+++ TS/Lesson_2/index.ts
@@ -1,2 +1,23 @@
+/**
+ * Khai báo kiểu dữ liệu với ":" cho string, number, blooean, object, array
+ */
+
 const myMessage: string =
   "Hom nay troi dep qua, rat vui duoc noi chuyen voi ban!";
+const myAge: number = 30;
+const isMarried: boolean = false;
+const product: {
+  name: string;
+  price: number;
+  isAvailable: boolean;
+  thumbnail: string;
+} = {
+  name: "Iphone 12",
+  price: 1000,
+  isAvailable: true,
+  thumbnail: "https://example.com/iphone12.jpg",
+};
+
+const myStudents: string[] = ["Dang", "Nam", "Bach", "Hoa", "Xanh"]; // Khai báo kiểu array type
+
+const myStudents2: Array<string> = ["Dang", "Nam", "Bach", "Hoa", "Xanh"]; // Khai báo kiểu generic type
