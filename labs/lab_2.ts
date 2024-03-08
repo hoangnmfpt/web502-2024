7a41dafda261aaa69e2c83ed802b0e5cdbe26660
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-08 15:46:29 +0700

    Lesson 3: Config react-typescript voi vite

--- labs/lab_2.ts
+++ labs/lab_2.ts
@@ -1,17 +1,17 @@
 {
   type productType = {
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    discountPercentage: number;
-    rating: number;
-    stock: number;
-    brand: string;
-    category: string;
-    thumbnail: string;
-    images: string[]; //! images: Array<string>
-  };
+    id: number
+    title: string
+    description: string
+    price: number
+    discountPercentage: number
+    rating: number
+    stock: number
+    brand: string
+    category: string
+    thumbnail: string
+    images: string[] //! images: Array<string>
+  }
 
   const product: productType = {
     id: 1,
@@ -29,87 +29,80 @@
       'https://cdn.dummyjson.com/product-images/1/2.jpg',
       'https://cdn.dummyjson.com/product-images/1/3.jpg',
       'https://cdn.dummyjson.com/product-images/1/4.jpg',
-      'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg',
-    ],
-  };
+      'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg'
+    ]
+  }
 }
 {
   type hairType = {
-    color: string;
-    type: string;
-  };
+    color: string
+    type: string
+  }
   type addressType = {
-    address: string;
-    city: string;
+    address: string
+    city: string
     coordinates: {
-      lat: number;
-      lng: number;
-    };
-    postalCode: string;
-    state: string;
-  };
+      lat: number
+      lng: number
+    }
+    postalCode: string
+    state: string
+  }
+  type companyType = {
+    address: addressType
+    department: string
+    name: string
+    title: string
+  }
+  type cryptoType = {
+    coin: string
+    wallet: string
+    network: string
+  }
   type bankType = {
-    cardExpire: string;
-    cardNumber: string;
-    cardType: string;
-    currency: string;
-    iban: string;
-  };
+    cardExpire: string
+    cardNumber: string
+    cardType: string
+    currency: string
+    iban: string
+  }
   type userType = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    maidenName: string;
-    age: number;
-    gender: string;
-    email: string;
-    phone: string;
-    username: string;
-    password: string;
-    birthDate: string;
-    image: string;
-    bloodGroup: string;
-    height: number;
-    weight: number;
-    eyeColor: string;
-    hair: hairType;
-    domain: string;
-    ip: string;
-    address: addressType;
-    macAddress: string;
-    university: string;
-    bank: bankType;
-    company: {
-      address: {
-        address: '629 Debbie Drive';
-        city: 'Nashville';
-        coordinates: {
-          lat: 36.208114;
-          lng: -86.58621199999999;
-        };
-        postalCode: '37076';
-        state: 'TN';
-      };
-      department: 'Marketing';
-      name: "Blanda-O'Keefe";
-      title: 'Help Desk Operator';
-    };
-    ein: '20-9487066';
-    ssn: '661-64-2976';
-    userAgent: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24';
-    crypto: {
-      coin: 'Bitcoin';
-      wallet: '0xb9fc2fe63b2a6c003f1c324c3bfa53259162181a';
-      network: 'Ethereum (ERC20)';
-    };
-  };
-  const user = {
+    id: number
+    firstName: string
+    lastName: string
+    maidenName: string
+    age: number
+    gender: 'male' | 'female' | 'other' // literal type - union type
+    email: string
+    phone: string
+    username: string
+    password: string
+    birthDate: string
+    image: string
+    bloodGroup: string
+    height?: number
+    weight?: number
+    eyeColor?: string
+    hair?: hairType
+    domain: string
+    ip: string
+    address: addressType
+    macAddress: string
+    university: string
+    bank: bankType
+    company: companyType
+    ein: string
+    ssn: string
+    userAgent: string
+    crypto: cryptoType
+  }
+  const user: userType = {
     id: 1,
     firstName: 'Terry',
     lastName: 'Medhurst',
     maidenName: 'Smitham',
     age: 50,
-    gender: 'male',
+    gender: 'other',
     email: 'atuny0@sohu.com',
     phone: '+63 791 675 8914',
     username: 'atuny0',
@@ -122,7 +115,7 @@
     eyeColor: 'Green',
     hair: {
       color: 'Black',
-      type: 'Strands',
+      type: 'Strands'
     },
     domain: 'slashdot.org',
     ip: '117.29.86.254',
@@ -131,10 +124,10 @@
       city: 'Washington',
       coordinates: {
         lat: 38.867033,
-        lng: -76.979235,
+        lng: -76.979235
       },
       postalCode: '20020',
-      state: 'DC',
+      state: 'DC'
     },
     macAddress: '13:69:BA:56:A3:74',
     university: 'Capitol University',
@@ -143,7 +136,7 @@
       cardNumber: '50380955204220685',
       cardType: 'maestro',
       currency: 'Peso',
-      iban: 'NO17 0695 2754 967',
+      iban: 'NO17 0695 2754 967'
     },
     company: {
       address: {
@@ -151,23 +144,23 @@
         city: 'Nashville',
         coordinates: {
           lat: 36.208114,
-          lng: -86.58621199999999,
+          lng: -86.58621199999999
         },
         postalCode: '37076',
-        state: 'TN',
+        state: 'TN'
       },
       department: 'Marketing',
       name: "Blanda-O'Keefe",
-      title: 'Help Desk Operator',
+      title: 'Help Desk Operator'
     },
     ein: '20-9487066',
     ssn: '661-64-2976',
-    userAgent:
-      'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24',
+    userAgent: 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24',
     crypto: {
       coin: 'Bitcoin',
       wallet: '0xb9fc2fe63b2a6c003f1c324c3bfa53259162181a',
-      network: 'Ethereum (ERC20)',
-    },
-  };
+      network: 'Ethereum (ERC20)'
+    }
+  }
+  console.log(user)
 }
