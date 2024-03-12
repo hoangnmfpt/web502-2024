4d43fac98d29ce4266468ff7f2d44c33798c21b3
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-12 11:09:43 +0700

    Lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,6 @@ function App() {
 				<img src={product.thumbnail} alt={product.title} />
 				<h5>Gia san pham: {product.price}</h5>
 			</div>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	)
 }
