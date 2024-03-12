5e95564e9af115390a4defa25b8400ec09c3ad4c
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-12 09:39:08 +0700

    Lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,49 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Product } from './types/Product'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
-  )
+	const [count, setCount] = useState(0)
+	const product: Product = {
+		id: 1,
+		title: 'iPhone 9',
+		description: 'An apple mobile which is nothing like apple',
+		price: 549,
+		discountPercentage: 12.96,
+		rating: 4.69,
+		stock: 94,
+		brand: 'Apple',
+		category: 'smartphones',
+		thumbnail: 'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg',
+		images: [
+			'https://cdn.dummyjson.com/product-images/1/1.jpg',
+			'https://cdn.dummyjson.com/product-images/1/2.jpg',
+			'https://cdn.dummyjson.com/product-images/1/3.jpg',
+			'https://cdn.dummyjson.com/product-images/1/4.jpg',
+			'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg'
+		]
+	}
+	return (
+		<>
+			<div>
+				<h2>{product.title}</h2>
+				<img src={product.thumbnail} alt={product.title} />
+				<h5>Gia san pham: {product.price}</h5>
+			</div>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	)
 }
 
 export default App
