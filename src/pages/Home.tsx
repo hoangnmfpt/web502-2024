ce792366a0847065a9eb01cbad64de8bea28ebee
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-13 17:25:07 +0700

    Lesson 5: React Component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductList from '~/components/ProductList'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Danh sach san pham</h2>
+      <ProductList />
+    </div>
+  )
+}
+
+export default Home
