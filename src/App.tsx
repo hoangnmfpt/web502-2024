d11ce35740b4753b443e4ad0c3cde03f233fb56e
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-14 09:56:57 +0700

    Lesson 5: JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import React from 'react';
 import './App.css'
-import { Product } from './types/Product'
 
 function App() {
-	const [count, setCount] = useState(0)
-	const product: Product = {
-		id: 1,
-		title: 'iPhone 9',
-		description: 'An apple mobile which is nothing like apple',
-		price: 549,
-		discountPercentage: 12.96,
-		rating: 4.69,
-		stock: 94,
-		brand: 'Apple',
-		category: 'smartphones',
-		thumbnail: 'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg',
-		images: [
-			'https://cdn.dummyjson.com/product-images/1/1.jpg',
-			'https://cdn.dummyjson.com/product-images/1/2.jpg',
-			'https://cdn.dummyjson.com/product-images/1/3.jpg',
-			'https://cdn.dummyjson.com/product-images/1/4.jpg',
-			'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg'
-		]
-	}
-	return (
-		<>
-			<div>
-				<h2>{product.title}</h2>
-				<img src={product.thumbnail} alt={product.title} />
-				<h5>Gia san pham: {product.price}</h5>
-			</div>
-		</>
-	)
+  console.log(typeof React.createElement(
+    "div",
+    { className: "app", style: { color: "red", backgroundColor: "blue"} },
+    React.createElement("h1", {className: "title"}, "Hello, World!")
+  ))
+    return React.createElement( 
+      "div",
+      { className: "app", style: { color: "red", backgroundColor: "blue"} },
+      React.createElement("h1", {className: "title"}, "Hello, World!")
+    );
+
+    return (
+      <div className="app" style={{color: "red"}}>
+        <h1 className ="title">Hello cac ban!</h1>
+        <button onClick={() => console.log("hi")}>Click Me</button>
+      </div>
+    )
+	
 }
 
 export default App
