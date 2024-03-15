a1f932475f521d7fadc2ec91edf15f66ab701a03
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-15 15:45:05 +0700

    remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    // ! strict mode //!chế độ nghiêm ngặt
     <App />
   </React.StrictMode>
 )
+
+// ! strict mode //!chế độ nghiêm ngặt
