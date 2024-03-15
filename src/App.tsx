f8dbcd4b742dfbc198d1bde0b8c30f67c653837d
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-15 16:55:25 +0700

    Lesson 6: Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
 import React from 'react'
 import './App.css'
-import Home from './pages/Home'
+import { TProduct } from './interfaces/TProduct'
+// import Home from './pages/Home'
+import { TProduct } from '~/interfaces/Product'
+
+const Home1 = (product: TProduct) => {
+  return (
+    <>
+      <h2>{product.title}</h2>
+      <p>Gia: {product.price}</p>
+    </>
+  )
+}
+
+type Props = { product: TProduct; name: string }
+
+//! props = propeties = Các thuộc tính
+const Home2 = (props: Props) => {
+  return (
+    <>
+      <h2>{props.product.title}</h2>
+      <p>Gia: {props.product.price}</p>
+      <p>Nhung san pham nay thuoc ve {props.name}</p>
+    </>
+  )
+}
 
 function App() {
-  console.log(typeof React.createElement('div', { className: 'app' }, React.createElement('h1', {}, 'Hello, World!')))
-
-  const addProduct = () => {
-    console.log('Them thanh cong!')
+  let product: TProduct = {
+    id: 1,
+    title: 'Iphone 12',
+    price: 1000,
+    description: 'New product'
   }
   return (
     <>
-      {/* <div className='app'>
-        <h1>Hello, World!</h1>
-        <button onClick={() => addProduct()}>Click Me</button>
-      </div> */}
-      <Home />
+      <h2>Hello</h2>
+      {Home1(product)}
+      <Home2 product={product} name={'Hoang'} />
+      <div>Day la phan quang cao</div>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import Banner from '~/components/Banner'
-import ProductList from '~/components/ProductList'
+import Banner from '@/components/Banner'
+import ProductList from '@/components/ProductList'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import { TProduct } from '@/interfaces/TProduct'
 
-const Home = () => {
+const Home = (product: TProduct) => {
   return (
     <div>
+      <Header />
       <Banner />
-      <h2>Danh sach san pham</h2>
+      <h2>Danh sach san pham:</h2>
       <ProductList />
+      <Footer />
     </div>
   )
 }
+
+// const sum = (a: number, b: number) => {
+//   return a + b
+// }
+
+// sum(2, 3)
 
 export default Home
