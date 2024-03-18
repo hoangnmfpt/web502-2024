dc0dcc1411c39002088f9063dc69d1aa57f918e7
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-18 13:19:18 +0700

    Lesson 7: useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -15,35 +15,33 @@ const App: React.FC = () => {
 		description: '',
 	});
 
-	const updateProduct = () => {
+	useEffect(() => {
 		fetch('http://localhost:3000/products/1')
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
 				setProduct(data);
 			});
-	};
+		return () => {
+			// cleanup function
+		};
+	}, []);
 
-	const Shop1 = (props: { product: TProduct }) => {
+	// cleanup with useEffect
+	/**
+	 * ! Không có dependency: cứ có sự thay đổi về state thì gọi lại callback.
+	 * ! [] : Chỉ chạy lại 1 lần componentDidMount
+	 * ! [state1, state2, ...] : Gọi lại khi một trong số các state được liệt kê có sự thay đổi.
+	 */
+	const Shop = (props: { product: TProduct }) => {
 		return (
 			<div>
-				<p>{props.name}</p>
 				<p>{props.product.id}</p>
 				<p>{props.product.title}</p>
 				<p>{props.product.price}</p>
 			</div>
 		);
 	};
-
-	// const shop2 = (product: TProduct) => {
-	// 	return (
-	// 		<>
-	// 			<p>{product.id}</p>
-	// 			<p>{product.title}</p>
-	// 			<p>{product.price}</p>
-	// 		</>
-	// 	);
-	// };
 
 	// ! UI Component = dumb component
 
@@ -52,9 +50,8 @@ const App: React.FC = () => {
 	return (
 		<div>
 			<Header />
-			<button onClick={() => updateProduct()}>Cap nhat san pham</button>
-			<Shop1 product={product} />
-			{/* {shop2(product)} */}
+			{/* <button onClick={() => updateProduct()}>Cap nhat san pham</button> */}
+			<Shop product={product} />
 			<Footer />
 		</div>
 	);
