4bb405dd132a26240a99bcd79c2372f2fdbf82f0
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-18 17:01:41 +0700

    Lesson 7: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import Footer from './components/Footer'
+import { Header } from './components/Header'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import NotFound from './pages/NotFound'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import ProductDetail from './pages/ProductDetail'
 
 function App() {
   return (
     <>
-      <Home />
-      <div>Day la phan quang cao</div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop/:id' element={<ProductDetail />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,15 @@
 import Banner from '@/components/Banner'
 import Footer from '@/components/Footer'
-import Header from '@/components/Header'
+import { Header } from '@/components/Header'
 import ProductList from '@/components/ProductList'
 
 type Props = {}
 const Home = (props: Props) => {
   return (
     <div>
-      <Header />
       <Banner />
       <h2>Danh sach san pham:</h2>
       <ProductList />
-      <Footer />
     </div>
   )
 }
