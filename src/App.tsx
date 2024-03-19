42535718e3d4e247afd015bf87b999510b89251f
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-19 10:58:16 +0700

    Lesson 8: router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import { Routes, Route } from 'react-router-dom';
+import Shop from './pages/Shop';
+import ProductDetail from "./pages/ProductDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Notfound from "./pages/Notfound";
 
 type Props = {};
 
@@ -11,7 +17,14 @@ const App = (props: Props) => {
     <>
       <Header />
       <Banner />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:id" element={<ProductDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Notfound = (props: Props) => {
+  return (
+    <div>Notfound</div>
+  )
+}
+
+export default Notfound
