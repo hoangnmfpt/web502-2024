91c5f6c36bdbfe8460ba92debc4a419d3ed598bf
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-21 09:42:11 +0700

    Lesson 8: Category

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,6 @@
-import { useEffect, useState } from "react";
 import ProductList from "~/components/ProductList";
-import { TProduct } from "~/interfaces/Product";
-import axios from "axios";
-import instance from "~/apis";
 
 const Home = () => {
-  /**
-   * ! Dependency với 3 trường hợp:
-   * ? undefined: không có dependency - re-render khi state được cập nhật.
-   * ? []: empty array - chỉ re-render khi componentDidMount.
-   * ? [state1, state2,...]: re-render khi một trong số các state được cập nhật.
-   */
-
   return (
     <div>
       <h2 className="sessionTitle">Danh cho nam:</h2>
