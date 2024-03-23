166315b93ba67461ff7e1a08f626413032ecab53
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-23 07:35:40 +0700

    Lesson 9: refactor

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import instance from "~/apis";
 import { TProduct } from "~/interfaces/Product";
+import ProductItem from "./ProductItem";
 
-type Props =   {
-  category: string
-}
-const ProductList = (props: Props) => {
-  console.log(props)
+const ProductList = () => {
   const [products, setProducts] = useState<TProduct[]>([]);
   useEffect(() => {
-    // Cach 2:
     const getAllProducts = async () => {
       try {
         const { data } = await instance.get(`/products`);
@@ -24,24 +19,13 @@ const ProductList = (props: Props) => {
   return (
     <div>
       <h1>Danh sach san pham</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <Link to={`/shop/${product.id}`}>
-            <h2>{product.title}</h2>
-          </Link>
-
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-          <p>{product.discountPercentage && "Dang cap nhat"}</p>
-          <p>{product.rating}</p>
-          <p>{product.stock}</p>
-          <p>{product.brand}</p>
-          <p>{product.category}</p>
-          <Link to={`/shop/${product.id}`}>
-            <img width={300} src={product.thumbnail} alt={product.title} />
-          </Link>
-        </div>
-      ))}
+      <div className="row">
+        {products.map((product) => (
+          <div className="col-4">
+            <ProductItem product={product} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,7 @@ import ProductList from "~/components/ProductList";
 const Home = () => {
   return (
     <div>
-      <h2 className="sessionTitle">Danh cho nam:</h2>
-      <ProductList category="nam" />
-      <br />
-      <h2 className="sessionTitle">Danh cho nu:</h2>
-      <ProductList category="nu" />
-      <br />
-      <h2 className="sessionTitle">Danh cho tre em:</h2>
-      <ProductList category="kid" />
+      <ProductList />
     </div>
   );
 };
