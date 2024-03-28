08f5b90796e6e71246f076a0423fd729f435a70f
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-28 09:08:06 +0700

    Lesson 11: Product Add

--- src/pages/admin/ProductAdd.tsx
+++ src/pages/admin/ProductAdd.tsx
@@ -3,7 +3,46 @@ import React from "react";
 type Props = {};
 
 const ProductAdd = (props: Props) => {
-  return <div>ProductAdd</div>;
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(e);
+  };
+  return (
+    <div>
+      <h2>Add product</h2>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="title"
+            id="title"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">Price</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="price"
+            id="price"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="description"
+            id="description"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ProductAdd;
